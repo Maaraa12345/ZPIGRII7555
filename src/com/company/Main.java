27b44d5ba16797555020093f7012cc5d6337f650
student <kamilxxl11@wp.pl>
2022-03-15 17:17:31 +0100

    Changes in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Pierwsza linia z galezi Main");
         System.out.println("Druga linia z galezi Main");
+        System.out.println("Trzecia linia z galezi Main");
+
     }
 }
