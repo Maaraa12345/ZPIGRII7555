8ee6011028a2bf7130d988c7f6c7f4b5d5ec48dd
student <kamilxxl11@wp.pl>
2022-03-15 17:02:52 +0100

    Changes in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("elo");
+        System.out.println("elo1");
 	// write your code here
     }
 }
